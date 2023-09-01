605e2c057cef3b2a8892e962babcde408914ff7f
yusluv1234 <yusluv124@gmail.com>
2023-09-01 20:54:56 +0100

    Adding some route

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,9 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
-
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <Modal title="Test" description="Test Desc" isOpen onClose={() => {}}>
-        Children
-      </Modal>
+      <div className="">Root Page</div>
     </div>
   );
 };
